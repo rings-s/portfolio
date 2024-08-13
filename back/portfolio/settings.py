a6c34551a3fc203a4130b8d6ad83035cffa53aa6
rings-s <rings9619@gmail.com>
2024-08-13 15:23:53 -0400

    setup and configure django cors headers to allow data to send to the backend

--- back/portfolio/settings.py
+++ back/portfolio/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "base",
     "rest_framework",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -123,3 +125,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
+CORS_ALLOWED_ORIGINS  = ["http://127.0.0.1:5173", "http://localhost:5173"]
