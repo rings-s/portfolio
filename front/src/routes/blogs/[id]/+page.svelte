df8d88078b351414f6a5849c87283aa01f8c92e4
rings-s <rings9619@gmail.com>
2024-08-16 06:56:54 -0400

    fixxing the blogs get functionality

--- front/src/routes/blogs/[id]/+page.svelte
+++ front/src/routes/blogs/[id]/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { BlogStore } from './../../BlogStore';
+	import { BlogStore } from '../../../BlogStore';
 	import { onMount } from 'svelte';
 
 	onMount(async () => {
@@ -18,14 +18,14 @@
 	});
 </script>
 
-	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+	<div class=" mt-12">
 		<div class="max-w-3xl mx-auto p-4 bg-white shadow rounded-lg ">
 			{#each $BlogStore as blog}
 				<div class="my-4 p-5 border-b last:border-b-0">
 					<h1 class="text-2xl font-bold text-gray-900">{blog.title}</h1>
-					<p class="text-sm text-gray-600">{blog.created_at}</p>
+					<p class="text-sm text-gray-800">{blog.description}</p>
 				</div>
-				<a href="/blogs/{blog.id}" class="text-gray-600">details</a>
+				<a href="/blogs/" class="text-gray-600">blogs</a>
 
 			{/each}
 		</div>
