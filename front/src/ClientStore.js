dab7c219dc597340317cf646063c38efe0acbbce
Ahmed Bashir <161429290+rings-s@users.noreply.github.com>
2024-08-30 13:55:27 -0400

    Update ClientStore.js

--- front/src/ClientStore.js
+++ front/src/ClientStore.js
@@ -5,9 +5,10 @@ import { writable } from "svelte/store";
 export const ClientStore = writable([
     {
         id: 0, 
-        name: "John Doe",
+        user: "John Doe",
         phone_number: "555-555-5555",
         // profile_image
+        email: 'ringo@yahoo.com',
         created_at: "2022-01-01"
     }
 ]);
